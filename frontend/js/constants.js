cc4d4bd78036d0436fe213a310c7107fe095e7bf
ermahster <97585790+ermahster@users.noreply.github.com>
2022-03-24 21:58:19 +0800

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x76499530550Dfc725eB081F552FF282E83CaDC9f"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
